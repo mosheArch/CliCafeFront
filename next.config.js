8dc3e6f13710b63c65aad5b8f40eb985b7ca1bee
mosheArch <alvaro.moises.diaz.diaz@gamil.com>
2024-12-15 19:58:48 -0600

    pago exitoso

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   images: {
     domains: ['api.clicafe.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://www.mercadopago.com https://http2.mlstatic.com;",
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
